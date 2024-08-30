91c4f4dc0f49cb134f16793d2a0d39c3fdd07975
Ben Visness <ben@bvisness.me>
2024-08-29 19:21:03 -0500

    Initial Commit

---
+++ ARTestUITests/ARTestUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  ARTestUITestsLaunchTests.swift
+//  ARTestUITests
+//
+//  Created by Ben Visness on 8/29/24.
+//
+
+import XCTest
+
+final class ARTestUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
